625b912d2246c46bb0377de8380fc84b55ddac3d
dharsha27 <dharshavardhan27@gmail.com>
2026-06-11 20:54:16 +0530

    Its second scripting

---
+++ 01-mongodb.sh
@@ -0,0 +1,40 @@
+#!/bin/bash 
+
+# Logs files
+
+LOGS_FOLDER="/var/log/roboshop"
+sudo mkdir -p "$LOGS_FOLDER"
+sudo chown -R ec2-user:ec2-user "$LOGS_FOLDER"
+sudo chmod 755 "$LOGS_FOLDER"
+SCRIPT_NAME=$(basename "$0")
+LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+
+USERID=$(id -u)
+
+# colours
+
+R="\e[31m]"
+G="\e[32m]"
+Y="\e[33m]"
+N="\e[0m]"
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+
+if [ $USERID -ne 0 ];then  
+      echo -e " $TIMESTAMP $R Please run script with root access...$N" | tee -a "$LOGS_FILE"
+      exit 1
+else
+     echo -e "$TIMESTAMP $G OK you are running with root acceess, go head..."
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then 
+       echo -e "$TIMESTAMP  [ERROR] $2 ....$R FAILURE $N " | tee -a "$LOGS_FILE"
+    else
+       echo -e "$TIMESTAMP  [INFO] $2 ....$G SUCCESS $N " | tee -a "$LOGS_FILE"
+    fi
+}
+
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Added Mongo repos"
