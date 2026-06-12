01be32e7d7d453bed8d7917f77ac0442d7f1bcb5
dharsha27 <dharshavardhan27@gmail.com>
2026-06-12 16:36:49 +0530

    oil and karam added

--- 02-roboshop.sh
+++ 02-roboshop.sh
@@ -55,26 +55,28 @@ do
 # }
 # "
 
-   aws route53 change-resource-record-sets --hosted-zone-id "$HOSTED_ZONE" --change-batch  "
+   aws route53 change-resource-record-sets \
+   --hosted-zone-id YOU_HOSTED_ZONE_ID \
+   --change-batch  '
    {
-     "Comment":"Update a new IP record"
+     "Comment": "Update a new IP record"
      "Changes": [
        {
-         "Action": "UPSERT",
-         "ResourceRecordSet": {
-         "Name": "'$R53_RECORD'",
-         "Type": "A",
-         "TTL": 1,
-         "ResourceRecords": [
+         " Action": " UPSERT ",
+         " ResourceRecordSet " : {
+         " Name ": "'$R53_RECORD'",
+         " Type ": "A" ,
+         " TTL": 1,
+         " ResourceRecords ": [
                { 
-                 "Value": "$IP"
+                 " Value": "$IP"
               }
             ]
           }
        }
     ]
 }
-"
+'
 
 done
 
