afc2259fe857ac8c72e8ed32db147e662477444b
dharsha27 <dharshavardhan27@gmail.com>
2026-06-13 13:36:13 +0530

    changes were made

--- roboshop-v2.sh
+++ roboshop-v2.sh
@@ -35,5 +35,43 @@ if [  "$ACTION" != "create"  ] && [  "$ACTION" != "delete"  ] ;then
      exit 1
 fi
 
+get_instance_id(){
+    name=$1
+    aws ec2 describe-instances --filters "Name=tag:Name,Values=roboshop-$name" "Name=instance-state-name, Values=running" --query "Reservations[0].Instances[0].InstanceId" --output text
 
+}
+
+launch_instance(){
+
+    echo "we are going to launching the  instance according to your requirement--roboshop $instance"
+    aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type t3.micro \
+    --security-groups "roboshop-common" "roboshop-$instance" \
+          --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]"\
+          --query 'Instances[0].InstanceId'\
+    --output text
+
+}
+
+for instance in "$@" 
+do 
+
+ INSTANCE_ID=$(get_instance_id "$instance")
+
+ if [ "$ACTION" == "create" ]; then
+     if [ "$INSTANCE_ID" == "None"  ]; then
+       
+              INSTANCE_ID=$(launch_instance) 
+              echo "Launched Instance: $INSTANCE_ID"
+
+     else
+          echo "roboshop-$instance already running : $INSTANCE_ID "
+     fi
+ fi          
+
+
+
+
+done
 
