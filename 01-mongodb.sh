94133b1eb7202237e82e5081dd33b427e387156a
dharsha27 <dharshavardhan27@gmail.com>
2026-06-13 16:27:12 +0530

    changes were made

--- 01-mongodb.sh
+++ 01-mongodb.sh
@@ -53,7 +53,7 @@ dnf install -y mongodb-org  &>> "$LOGS_FILE"
 # Adjust Configuration for Remote Access and Disable Protected Mode
 
 echo -e "$(date "+%Y-%m-%d %H:%M:%S") [INFO] Editing /etc/redis/redis.conf settings..." | tee -a "$LOGS_FILE"
-sed -i -e 's/bind 127.0.0.1/bind 0.0.0.0/g'  /etc/mongod.conf &>> "$LOGS_FILE" 
+sed -i -e 's/127.0.0.1/0.0.0.0/g'  /etc/mongod.conf &>> "$LOGS_FILE" 
 VALIDATE $? "Allowing remote changes in config file"
 
 
