54d7994cc068011b2350fee5b6f1e952e586ac6b
dharsha27 <dharshavardhan27@gmail.com>
2026-06-14 13:51:20 +0530

    Changes were made

--- roboshop.sh
+++ roboshop.sh
@@ -31,7 +31,7 @@ get_instance_id(){
     aws ec2 describe-instances --filters "Name=tag:Name,Values=roboshop-$name" "Name=instance-state-name,Values=running" --query "Reservations[0].Instances[0].InstanceId" --output text
 }
 
-for instance in $@
+for instance in "$@"
 do
     INSTANCE_ID=$(get_instance_id $instance)
     if [ $ACTION == "create" ]; then
